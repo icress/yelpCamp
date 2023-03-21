eb281b6c7ab5baf41f2fa0827144583cf7424854
icres <icress26@gmail.com>
2023-03-21 15:59:59 -0600

    Removed ejs-mate and addressed issues with css

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -14,7 +13,6 @@ db.once('open', () => {
     console.log('DATABASE CONNECTED')
 })
 
-app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/style'));
